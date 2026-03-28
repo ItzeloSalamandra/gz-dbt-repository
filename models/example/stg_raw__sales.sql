bd4c77342e6205dd63873bdceeb1b8f2093a123c
ramirez.itzel@uabc.edu.mx <ramirez.itzel@uabc.edu.mx>
2026-03-28 20:12:01 +0000

    Added_stg_raw_sales

---
+++ models/example/stg_raw__sales.sql
@@ -0,0 +1,15 @@
+with sales_source as (
+    select * from {{ source('raw', 'sales') }}
+),
+
+renamed as (
+    select
+        date_date,
+        orders_id,
+        pdt_id AS products_id,
+        revenue,
+        quantity
+    from sales_source
+)
+
+select * from renamed   
