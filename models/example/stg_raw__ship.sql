87bc01b38d2f31c5fc3211b9c130ea7c1c24c79e
ramirez.itzel@uabc.edu.mx <ramirez.itzel@uabc.edu.mx>
2026-03-28 20:31:00 +0000

    Add_3_ables

---
+++ models/example/stg_raw__ship.sql
@@ -0,0 +1,14 @@
+with ship_source as (
+    select * from {{ source('raw', 'ship') }}
+),
+
+renamed as (
+    select
+        orders_id,
+        shipping_fee,
+        logCost,
+        CAST(ship_cost AS INT64) AS ship_cost
+    from ship_source
+)
+
+select * from renamed   
