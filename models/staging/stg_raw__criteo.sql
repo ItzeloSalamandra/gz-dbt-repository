b23d7099a5c1a7cc81d1dc4224194fca548afc52
ITZEL RAMIREZ SALAZAR <ramirez.itzel@uabc.edu.mx>
2026-04-03 00:35:34 +0000

    fixing

---
+++ models/staging/stg_raw__criteo.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table') }}
+
+with source as (
+    select * from {{ source('raw', 'criteo') }} -- Cambia esto por bing, facebook o criteo según el archivo
+),
+
+renamed as (
+    select
+        -- Creamos o traemos la llave de la campaña
+        concat(paid_source, '_', camPGN_name) as campaign_key,
+        date_date,
+        paid_source,
+        campaign_key
+        camPGN_name as campaign_name, -- Renombrado solicitado
+        CAST(ads_cost AS float64) as ads_cost, -- Cast solicitado
+        impression,
+        click
+    from source
+)
+
+select * from renamed
