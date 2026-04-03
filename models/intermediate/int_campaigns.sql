ee174687199e9349aca06422780f76c066d754c3
ITZEL RAMIREZ SALAZAR <ramirez.itzel@uabc.edu.mx>
2026-04-03 00:17:56 +0000

    int_campaign

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,12 @@
+with union_campaigns as (
+    SELECT * FROM {{ ref('stg_raw__adwords') }}
+    UNION ALL
+    SELECT * FROM {{ ref('stg_raw__bing') }}
+    UNION ALL
+    SELECT * FROM {{ ref('stg_raw__facebook') }}
+    UNION ALL
+    SELECT * FROM {{ ref('stg_raw__criteo') }}
+)
+
+select *
+from union_campaigns
