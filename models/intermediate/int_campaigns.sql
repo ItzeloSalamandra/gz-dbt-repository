f013298a0ac7075b2cf4be8e67850008468310a9
ITZEL RAMIREZ SALAZAR <ramirez.itzel@uabc.edu.mx>
2026-04-03 00:25:42 +0000

    fixing

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,6 +1,6 @@
 -- models/int_campaigns.sql
 with union_campaigns as (
-    SELECT * FROM {{ ref('stg_raw__adwords') }} -- Un solo guion
+    SELECT * FROM {{ ref('stg_raw__adwords') }}
     UNION ALL
     SELECT * FROM {{ ref('stg_raw__bing') }}
     UNION ALL
